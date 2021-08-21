5ebce1fb1a45039146d4aa09ff98c15ab121fdfe
AnimeshKatigar <katigaranimesh@gmail.com>
2021-08-21 16:21:52 +0530

    Image loaders

--- src/pages/Cards.jsx
+++ src/pages/Cards.jsx
@@ -7,6 +7,7 @@ import { AiFillGithub } from "react-icons/ai";
 import { projects, intprojects } from "../assets/data/Data";
 
 const Cards = () => {
+  
   return (
     <div className="CardMainContainer">
       <Particle />
--- src/pages/ProjectCards.js
+++ src/pages/ProjectCards.js
@@ -1,11 +1,40 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import Card from "react-bootstrap/Card";
 
-
 function ProjectCards(props) {
+  const [loading, setLoading] = useState(true);
+  const counter = useRef(0);
+  
   return (
     <Card className="project-card-view">
-      <Card.Img variant="top" src={props.imgPath} alt="card-img" style={{height:160}}/>
+      <div
+        className="loadingDiv"
+        style={{
+          display: loading ? "flex" : "none",
+          height: 100,
+          backgroundColor: "transparent",
+        }}
+      >
+        <div class="loader-3">
+          <div class="dot dot1"></div>
+          <div class="dot dot2"></div>
+          <div class="dot dot3"></div>
+        </div>
+      </div>
+
+      <Card.Img
+        variant="top"
+        src={props.imgPath}
+        alt="card-img"
+        style={{ height: 160, display: loading && "none" }}
+        onLoad={() => {
+          counter.current += 1;
+          if (counter.current >= 1) {
+            setLoading(false);
+          }
+        }}
+      />
+
       <Card.Body>
         <Card.Title>
           <h3>{props.title}</h3>
@@ -15,10 +44,10 @@ function ProjectCards(props) {
         </Card.Text>
         {props.codeLink && (
           <div className="btn">
-          <a href={props.codeLink} target="_blank" rel="noreferrer">
-            Code
-          </a>
-        </div>
+            <a href={props.codeLink} target="_blank" rel="noreferrer">
+              Code
+            </a>
+          </div>
         )}
       </Card.Body>
     </Card>
